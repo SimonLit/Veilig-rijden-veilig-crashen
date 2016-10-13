07e27417e618ab42af6949fffd5f295e8ccea99c
MarinoBrundu <m.brundu@hotmail.com>
2016-10-13 12:11:59 +0200

    work in progress

---
+++ software/hulpdiensten_verzekering/hulpdiensten_verzekering/InsuranceRapport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hulpdiensten_verzekering
+{
+    public class InsuranceRapport : Rapport
+    {
+        public string InsuranceCompany { get; private set; }
+
+        private int victimID;
+        public override int VictimID
+        {
+            get
+            {
+                return victimID;
+            }
+
+            set
+            {
+            }
+        }
+
+        public int insuranceRapportNumber { get; private set; }
+
+        public string InsuranceDetails { get; private set; }
+
+        public int ClaimNr { get; private set; }
+    }
+}
